d9bd6d80a7b60af9c74f51dbfe1404ec45aacb20
Chen Lay <maungmaungyoker.mmy@gmail.com>
2021-10-25 21:10:56 +0630

    Removove Unwanted Sensitive Data :pushpin:

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,10 +1,14 @@
 const admin = require('firebase-admin')
-const serviceAccount = require('./serviceAccount.json')
+// const serviceAccount = require('./serviceAccount.json')
+
+const service_account = process.env.NEXT_PUBLIC_SERVICE_ACCOUNT
+
+// console.log(service_account)
 
 export const verifyIdToken = (token) => {
 	if (!admin.apps.length) {
 		admin.initializeApp({
-			credential: admin.credential.cert(serviceAccount),
+			credential: admin.credential.cert(service_account),
 		})
 	}
 	return admin
